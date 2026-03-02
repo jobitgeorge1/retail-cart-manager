1d13e2c8cd79d188420d4eda4474b4fd0fc66b21
Jobit George <Jobitpgeorge@gmail.com>
2026-03-02 16:56:45 +1100

    feat: add installable iPhone PWA support with manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "personal-cart-manager-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./app.html",
+  "./styles.css",
+  "./login.js",
+  "./app.js",
+  "./appwrite-client.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
